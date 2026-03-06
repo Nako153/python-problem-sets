35e317bf4e952bee55a551de1107fcaf54780038
Nako153 <nakonarimanishvili2008@gmail.com>
2026-03-06 18:18:46 +0000

    faces solution

--- meal/meal.py
+++ meal/meal.py
@@ -2,9 +2,9 @@ def main():
     time = input("What time is it? ")
     k = convert(time)
 
-    if 7 <= k <= 11:
+    if 7 <= k <= 8:
         print("breakfast time")
-    elif 12 <= k <= 17:
+    elif 12 <= k <= 13:
         print("lunch time")
     elif 18 <= k <= 19:
         print("dinner time")
