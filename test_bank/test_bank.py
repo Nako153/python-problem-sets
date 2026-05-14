26420c5c2ec4a3df0906dcd308eecbc37c4a792f
Nako153 <nakonarimanishvili2008@gmail.com>
2026-05-14 17:12:34 +0000

    add test_bank.py

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,1 +1,17 @@
 from bank import value
+
+
+def test_hello():
+    assert value("hello") == 0
+    assert value("hello, Newman") == 0
+    assert value("HELLO") == 0
+
+
+def test_h():
+    assert value("hi") == 20
+    assert value("How you doing?") == 20
+
+
+def test_other():
+    assert value("What's happening?") == 100
+    assert value("Good morning") == 100
